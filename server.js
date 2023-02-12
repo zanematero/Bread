056ce6ce5cbcce8839df293b60eac7a50ee0b373
Zane Matero <materozane@gmail.com>
2023-02-12 10:58:59 -0800

    create new routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')

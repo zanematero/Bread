1009bdee35ed25ef7e792a71ae1f3e1bcadbafaa
Zane Matero <materozane@gmail.com>
2023-02-25 11:31:21 -0800

    created baker model

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+const Baker = require('../models/baker.js')
 
 // INDEX
 breads.get('/', (req, res) => {
@@ -15,8 +16,14 @@ breads.get('/', (req, res) => {
 
 // NEW
 breads.get('/new', (req, res) => {
-  res.render('new')
+    Baker.find()
+        .then(foundBakers => {
+            res.render('new', {
+                bakers: foundBakers
+            })
+      })
 })
+
 
 // SHOW
 breads.get('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -24,11 +24,15 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
   
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
+// breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
 
-  // 404 Page
+// bakers 
+const bakersController = require('./controllers/bakers_controller.js')
+app.use('/bakers', bakersController)
+
+// 404 Page
 app.get('*', (req, res) => {
   res.send('404')
 })
